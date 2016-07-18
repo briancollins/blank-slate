5fe27ef559307d1ce9a062922de8eac6239b9613
Brian Collins <brian@stripe.com>
2016-07-18 10:55:26 +0100

    Create a charge

--- server.rb
+++ server.rb
@@ -9,3 +9,13 @@ Stripe.api_key = ENV['SECRET_KEY']
 get '/' do
   erb :index
 end
+
+post '/charge' do
+  Stripe::Charge.create(
+    :amount => 2000,
+    :currency => "gbp",
+    :source => params[:stripeToken],
+    :description => "Charge for test@example.com"
+  )
+  "Thanks for the cash!"
+end
